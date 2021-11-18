4b9cf8641e0014a4f9c00b2d5255d31bb9d4b8ca
kiran <saikiran.rondla@gmail.com>
2021-11-18 16:48:06 +0530

    Update TwoSum.java

--- src/ds/array/TwoSum.java
+++ src/ds/array/TwoSum.java
@@ -5,6 +5,24 @@ public class TwoSum {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        
+        int[] arr = {1,2,3,4,5};
+        int k = 9;
+        
+        //check if there are any two numbers in the given list that sum up to given number in this case "k"
+        // try and do it in O(1)
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i = 0 ; i < arr.length ; i++){
+            
+
+
+            
+        }
+        
+        
+        
 
 
     }
